99888c6f1b4ed041d5d64a992d42c620d1dafc3a
Emil <martinov.emil@gmail.com>
2023-10-29 14:43:58 +0300

    Lusitana font fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo";
-import { lusitana } from "./ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 
 import Link from "next/link";
